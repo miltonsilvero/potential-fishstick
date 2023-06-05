58ac371c57e0ca857b4ebed76615010cd2c8a47a
miltonsilvero <134976766+miltonsilvero@users.noreply.github.com>
2023-06-05 13:06:09 -0300

    Clase 2-I

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,59 @@ const app = new Application({
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 556788,
-	width: 600,
+	width: 870,
 	height: 400
 });
 
+window.addEventListener("resize", ()=>{
+	console.log("resized!");
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+
+	const scale = Math.min(scaleX, scaleY);
+
+	const gameWidht = Math.round(app.screen.width * scale);
+	const gameHeight = Math.round(app.screen.height * scale);
+
+	const marginHor = Math.floor((window.innerWidth - gameWidht)/2);
+	const marginVer = Math.floor((window.innerHeight - gameHeight)/2);
+
+	const canvas = app.view as HTMLCanvasElement;
+
+    canvas.style.width = gameWidht + "px";
+    canvas.style.height = gameHeight + "px";
+
+    canvas.style.marginLeft = marginHor + "px";
+    canvas.style.marginRight = marginHor + "px";
+
+    canvas.style.marginTop = marginVer + "px";
+    canvas.style.marginBottom = marginVer + "px";
+});
+window.dispatchEvent(new Event("resize"));
+
+
+
 Assets.add("myFrog", "./frog.png");
+Assets.add("Hat", "./frogHat.png");
+
 
 Assets.load(["myFrog"]).then(()=>{
-	const clampy: Sprite = Sprite.from("myFrog");
+	const frog: Sprite = Sprite.from("myFrog");
 
-	console.log("Hola Mundo", clampy.width, clampy.height);
+	console.log("Hola Mundo", frog.width, frog.height);
 
-	clampy.anchor.set(0.5);
+	//clampy.anchor.set(0.5);
 	
-	clampy.x = app.screen.width / 2;
-	clampy.y = app.screen.height / 2;
+	frog.x = 50;
+	frog.y = 50;
 	
-	app.stage.addChild(clampy);
+	frog.scale.x = 0.7;
+	frog.scale.y = 0.7;
+
+	const hat: Sprite = Sprite.from("Hat");
+
+	app.stage.addChild(hat);
+	app.stage.addChild(frog);
 
 });
 
